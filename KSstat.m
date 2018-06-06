47ef2237c93dd30278f06aaf93ddd9623698d4f0
Stephen Zhang <x.stephen.zhang@gmail.com>
2018-06-06 16:49:23 -0400

    Initial commit

--- KSstat.m
+++ KSstat.m
@@ -1,25 +1,14 @@
-function [output, D] = KSstat(cdf1, cdf2, N1, N2)
+function [p_value, D] = KSstat(cdf1, cdf2, N1, N2)
 %KSstat gives a significance statement on given cdfs.
-%   [output, D] = KSstat(cdf1, cdf2, N1, N2)
+%   [p_value, D] = KSstat(cdf1, cdf2, N1, N2)
 
-% Significance thresholds for different alpha values (0.05, 0.01, 0.001)
-thresh = [1.36, 1.63, 1.95];
-
-% Adjust for sample size
-thresh2 = thresh * sqrt((N1+N1)/N1/N2);
-
-% Calculate D
+% Calculate max deviation
 D = max(abs(cdf1 - cdf2));
 
-% Output
-if D > thresh2(3)
-    output = 'p < 0.001';
-elseif D > thresh2(2)
-    output = 'p < 0.01';
-elseif D > thresh2(1)
-    output = 'p < 0.05';
-else
-    output = 'not significant';
-end
+% Adjust for sample size
+C = D / sqrt((N1+N1)/N1/N2);
+
+% p value
+p_value = exp(-2 * C^2) * 2;
 end
 
